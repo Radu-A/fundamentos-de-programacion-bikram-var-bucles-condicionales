d602a11ad5ad7b1a7ab5fd9430a9c6c95fa1a240
Radu-A <elcastravete@gmail.com>
2023-04-21 14:53:15 +0200

    primeros ejercicios

---
+++ index.js
@@ -0,0 +1,24 @@
+let variableSinValor;
+
+let booleano1 = true;
+let booleano2 = false;
+
+const PI = 3.14;
+
+const TAU = 2 * PI;
+
+const booleanoAnd = booleano1 && booleano2;
+
+const booleanoNot = !booleano1;
+
+const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2))
+
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+
+let contarHasta10_2 = 0;
+
+console.log(contarHasta10_2);
